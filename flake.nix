8e374c251509b25fc77b4d5a9221b28cf7be325d
Artyom <artemtiunelis@gmail.com>
2025-07-09 17:49:59 +0100

    got first flake working

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "Laptop NixOS configuration.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 
-    nixosConfiguration.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux"; # Change if laptop is ARM
       specialArgs = { inherit self; };
       
