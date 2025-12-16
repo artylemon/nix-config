23ff7a278cdcba39c34a3c8ba80284b368e5d6e9
artylemon <artemtiunelis@gmail.com>
2025-12-16 21:11:42 +0000

    fixing the reboot hangs

--- hosts/nixos/boot.nix
+++ hosts/nixos/boot.nix
@@ -3,4 +3,8 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # Fix for stuck shutdown/reboot (watchdog not stopping)
+  boot.kernelParams = [ "nowatchdog" ];
+  boot.blacklistedKernelModules = [ "iTCO_wdt" ];
 }
